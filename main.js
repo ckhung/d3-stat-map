a553c0b27e089db29398be9ecae98da17109896e
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2015-10-06 12:23:36 +0800

    max = min + span - 1

--- main.js
+++ main.js
@@ -1,6 +1,28 @@
-d3.select('#slider3').call(d3.slider().axis(true).
-    value( [ 10, 25 ] ).on("slide", function(evt, value) {
-	d3.select('#slider3textmin').text(value[ 0 ]);
-	d3.select('#slider3textmax').text(value[ 1 ]);
-    })
-);
+d3.select('#slider_age_min').call(d3.slider().axis(true).min(0).max(100).
+    step(5).on("slide", onAgeMinChange));
+
+d3.select('#slider_age_span').call(d3.slider().axis(true).min(5).max(100).
+    step(5).on("slide", onAgeSpanChange));
+
+function onAgeMinChange(evt, value) {
+    d3.select('#text_age_min').text(value);
+    n = Number(value) + Number(d3.select('#text_age_span').text()) - 1;
+    d3.select('#text_age_max').text(n);
+}
+
+function onAgeSpanChange(evt, value) {
+    d3.select('#text_age_span').text(value);
+    n = Number(d3.select('#text_age_min').text()) + Number(value) - 1;
+    d3.select('#text_age_max').text(n);
+}
+
+var census_data;
+
+function main() {
+    d3.json("census-town.json", function(error, data) {
+	if (error) return console.warn(error);
+	census_data = data;
+    });
+}
+
+main();
