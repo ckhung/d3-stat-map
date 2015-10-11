71adfdd84abb73cef07324173fa301c01cd94b49
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2015-10-11 22:02:45 +0800

    census_data: hash => plain array

--- main.js
+++ main.js
@@ -6,14 +6,14 @@ d3.select('#slider_age_span').call(d3.slider().axis(true).min(1).max(101).
 
 function onAgeMinChange(evt, value) {
     d3.select('#text_age_min').text(value);
-    n = Number(value) + Number(d3.select('#text_age_span').text()) - 1;
+    var n = Number(value) + Number(d3.select('#text_age_span').text()) - 1;
     d3.select('#text_age_max').text(n);
     refresh_all();
 }
 
 function onAgeSpanChange(evt, value) {
     d3.select('#text_age_span').text(value);
-    n = Number(d3.select('#text_age_min').text()) + Number(value) - 1;
+    var n = Number(d3.select('#text_age_min').text()) + Number(value) - 1;
     d3.select('#text_age_max').text(n);
     refresh_all();
 }
@@ -21,72 +21,99 @@ function onAgeSpanChange(evt, value) {
 var census_data;
 
 function pop_ratio(d) { 
-    min_age = Number(d3.select('#text_age_min').text());
-    age_span = Number(d3.select('#text_age_span').text());
-    town = d.key;
-    upper = min_age+age_span;
+    var min_age = Number(d3.select('#text_age_min').text());
+    var age_span = Number(d3.select('#text_age_span').text());
+    var upper = min_age+age_span;
     if (upper > 101) upper = 101;
-    ratio = (census_data[town]['男'][upper]
-	  + census_data[town]['女'][upper]
-	  - census_data[town]['男'][min_age]
-	  - census_data[town]['女'][min_age] )
-	  / (census_data[town]['男'][101] + census_data[town]['女'][101]);
-    return (ratio*width).toString() + 'px';
+    return (d['男'][upper] + d['女'][upper]
+	  - d['男'][min_age] - d['女'][min_age] )
+	  / (d['男'][101] + d['女'][101]);
 }
 
 function refresh_bar_chart() {
-    bar_chart = d3.select('#bar_chart_proper');
-    width = parseInt(bar_chart.style('width')) - 140;
-    // https://stackoverflow.com/questions/13808741/bar-chart-with-d3-js-and-an-associative-array
-    // Bar chart with d3.js and an associative array
-    divs = bar_chart.selectAll('.entry').data(
-	d3.entries(census_data), function(d) { return d.key; }
+    var bar_chart = d3.select('#bar_chart_proper');
+    var width = parseInt(bar_chart.style('width')) - 140;
+    var divs = bar_chart.selectAll('.entry').data(
+	census_data, function(d) { return d.name; }
     );
 
     divs.exit().remove();
 
     // https://stackoverflow.com/questions/13203897/d3-nested-appends-and-data-flow
-    new_entries = divs.enter()
+    var new_entries = divs.enter()
 	.append('div')
 	.attr('class', 'entry');
     new_entries.append('div')
 	.attr('class', 'entry_text')
-	.html(function(d) { return d.key; });
+	.html(function(d) { return d.name; });
     new_entries.append('div')
 	.attr('class', 'entry_bar')
 	.html('&nbsp;');
 
     divs.select('.entry_bar').transition()
-	.style('width', pop_ratio);
+	.style('width', function (d) {
+	    return (pop_ratio(d)*width).toString() + 'px';
+	});
 }
 
 function refresh_gender_plot() {
-    width = parseInt(d3.select('#gender_plot_panel').style('width'));
-    svg = d3.select('#gender_plot_panel').select('svg');
-    svg.attr('width', width).attr('height', width);
-    svg.append('rect')
+    var width = parseInt(d3.select('#gender_plot_panel').style('width'));
+    var svg = d3.select('#gender_plot_panel').select('svg');
+    var height = width;
+    svg.attr('width', width).attr('height', height);
+    svg.select('#background')
 	.attr('width', '100%')
-	.attr('height', '100%')
-	.attr('fill', '#eef');
+	.attr('height', '100%');
+    var x = d3.scale.linear()
+	.range([0, width])
+	.domain(Object.keys(census_data).map(function (k) {
+	    return pop_ratio(census_data[k]);
+	}));
+    var y = x;
+    
+/*
+    var towns = svg.selectAll('text.town').data(
+	d3.entries(census_data), function(d) { return d.key; }
+    );
+
+    towns.exit().remove();
+
+    var new_towns = svg.enter()
+	.append('text')
+	.attr('class', 'town')
+	.text(function (d) { return d.key; });
+
+    towns.select('text.town').transition()
+	.attr('x', pop_ratio)
+	.attr('y', pop_ratio);
+*/
 }
 
-function refresh_all(e) {
+function refresh_all() {
     refresh_bar_chart();
-    refresh_gender_plot();
+//    refresh_gender_plot();
 }
 
 d3.json('census-taichung.json', function(error, data) {
     if (error) return console.warn(error);
     census_data = data;
-    for (var town in census_data) {
-	census_data[town]['男'].unshift(0);
-	census_data[town]['女'].unshift(0);
-    }
+    census_data.forEach(function (d) {
+	d['男'].unshift(0);
+	d['女'].unshift(0);
+    });
 
-    d3.select("#gender_plot_panel").append("svg");
+    var svg = d3.select("#gender_plot_panel").append("svg");
+    svg.append('rect')
+	.attr('id', 'background')
+	.attr('fill', '#eef');
 
     d3.select(window).on('resize', refresh_all); 
     d3.selectAll('button.div-switch').on('click.refresh', refresh_all); 
     refresh_all();
 });
 
+    // https://stackoverflow.com/questions/13808741/bar-chart-with-d3-js-and-an-associative-array
+    // Bar chart with d3.js and an associative array
+//    var divs = bar_chart.selectAll('.entry').data(
+//        d3.entries(census_data), function(d) { return d.key; }
+//    );
