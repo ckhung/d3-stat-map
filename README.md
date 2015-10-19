2ac2779eeb4260610d5f5fb7bfec239fc56b67a7
Chao-Kuei Hung <ckhung@cyut.edu.tw>
2015-10-19 18:51:07 +0800

    read map info (town boundary)

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 using d3 to create statistics graphs and statistics maps
 
 詳見 [專案官網](http://ckhung.github.com/d3-stat-map/index.zh_TW.html)
-及 [統計圖頁面](http://ckhung.github.com/d3-stat-map/main.html))
+及 [統計圖頁面](http://ckhung.github.com/d3-stat-map/main.html)
--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-d3.select('#slider_age_min').call(d3.slider().axis(true).min(0).max(100).
-    step(1).on('slide', onAgeMinChange));
-
-d3.select('#slider_age_span').call(d3.slider().axis(true).min(1).max(101).
-    step(1).on('slide', onAgeSpanChange));
+var census_data = null;
+var town_boundary = null;
 
 function onAgeMinChange(evt, value) {
     d3.select('#text_age_min').text(value);
@@ -18,16 +15,14 @@ function onAgeSpanChange(evt, value) {
     refresh_all();
 }
 
-var census_data;
-
 function pop_ratio(d) { 
     var min_age = Number(d3.select('#text_age_min').text());
     var age_span = Number(d3.select('#text_age_span').text());
     var upper = min_age+age_span;
     if (upper > 101) upper = 101;
-    return (d['男'][upper] + d['女'][upper]
-	  - d['男'][min_age] - d['女'][min_age] )
-	  / (d['男'][101] + d['女'][101]);
+    return (d['男'][upper] + d['女'][upper] -
+	  d['男'][min_age] - d['女'][min_age] ) /
+	  (d['男'][101] + d['女'][101]);
 }
 
 function male_binomial_z(d) { 
@@ -37,9 +32,9 @@ function male_binomial_z(d) {
     if (upper > 101) upper = 101;
     var m = d['男'][upper] - d['男'][min_age];
     var f = d['女'][upper] - d['女'][min_age];
-    return (m == 0) ?
-	((f == 0) ? 0 : -9) :
-	((f == 0) ? 9 : (m-f)/2*Math.sqrt((m+f)/m/f));
+    return (m === 0) ?
+	((f === 0) ? 0 : -9) :
+	((f === 0) ? 9 : (m-f)/2*Math.sqrt((m+f)/m/f));
     // http://homepages.wmich.edu/~bwagner/StatReview/Binomial/Binomial%20Hyp.htm
 }
 
@@ -75,7 +70,7 @@ function refresh_gender_plot() {
     d3.select('#gender_plot_panel svg').
 	attr('width', width).
 	attr('height', height);
-    var canvas = d3.select('#canvas');
+    var canvas = d3.select('#gp_canvas');
     canvas.select('#background')
 	.attr('width', '100%')
 	.attr('height', '100%');
@@ -83,7 +78,7 @@ function refresh_gender_plot() {
     var sx = d3.scale.linear()
 	.range([0, width])
 	.domain([d3.min(data_values), d3.max(data_values)]);
-    var data_values = census_data.map(male_binomial_z);
+    data_values = census_data.map(male_binomial_z);
     var sy = d3.scale.linear()
 	.range([height*0.9, height*0.1])
 	.domain([d3.min(data_values), d3.max(data_values)]);
@@ -107,7 +102,7 @@ function refresh_gender_plot() {
 
     towns.transition()
 	.attr('x', function(d) { return sx(pop_ratio(d)); } )
-	.attr('y', function(d) { return sy(male_binomial_z(d)); } )
+	.attr('y', function(d) { return sy(male_binomial_z(d)); } );
 
     var axisX = d3.svg.axis().scale(sx).orient("bottom"),
 	axisY = d3.svg.axis().scale(sy).orient("left");
@@ -119,9 +114,13 @@ function refresh_gender_plot() {
 	.call(axisY);
 }
 
+function refresh_pop_map() {
+}
+
 function refresh_all() {
     refresh_bar_chart();
     refresh_gender_plot();
+    refresh_pop_map();
 }
 
 function create_axes() {
@@ -130,36 +129,93 @@ function create_axes() {
 	sy = d3.scale.linear().domain([-3,3]).range([0,600]),
 	axisX = d3.svg.axis().scale(sx).orient('bottom'),
 	axisY = d3.svg.axis().scale(sy).orient('left'),
-	canvas = d3.select('#canvas');
+	canvas = d3.select('#gp_canvas');
     canvas.append('g').attr('id', 'x_axis');
     canvas.append('g').attr('id', 'y_axis');
 }
-    
-d3.json('census-taichung.json', function(error, data) {
-    if (error) return console.warn(error);
-    census_data = data;
-    census_data.forEach(function (d) {
-	d['男'].unshift(0);
-	d['女'].unshift(0);
-    });
 
-    var zoomListener = d3.behavior.zoom()
+function gpzh() {
+// gender plot zoom handler
+    d3.select('#gp_canvas').attr('transform', 'translate(' +
+	d3.event.translate + ')scale(' + d3.event.scale + ')');
+}
+
+function pmzh() {
+// population map zoom handler
+    d3.select('#pm_canvas').attr('transform', 'translate(' +
+	d3.event.translate + ')scale(' + d3.event.scale + ')');
+}
+
+function init() {
+    d3.select('#slider_age_min').call(d3.slider().axis(true).min(0).max(100).
+	step(1).on('slide', onAgeMinChange));
+
+    d3.select('#slider_age_span').call(d3.slider().axis(true).min(1).max(101).
+	step(1).on('slide', onAgeSpanChange));
+
+    // gender plot zoom listener
+    var gpzl = d3.behavior.zoom()
 	.scaleExtent([0.2, 8])
-	.on('zoom', function() {
-	    canvas.attr('transform', 'translate(' +
-		d3.event.translate + ')scale(' + d3.event.scale + ')');
-	});
+	.on('zoom', gpzh);
     // http://bl.ocks.org/cpdean/7a71e687dd5a80f6fd57
     var canvas = d3.select('#gender_plot_panel').
-	append('svg').call(zoomListener).append('g').attr('id', 'canvas');
+	append('svg').call(gpzl).append('g').attr('id', 'gp_canvas');
     canvas.append('rect')
 	.attr('id', 'background')
 	.attr('fill', '#eef');
     create_axes();
 
+//    https://stackoverflow.com/questions/14492284/center-a-map-in-d3-given-a-geojson-object
+    var width = 600;
+    var height = width*1.2;
+    var projection = d3.geo.mercator().scale(1).translate([0, 0]);
+    var path = d3.geo.path().projection(projection);
+    var b = path.bounds(town_boundary),
+	s = 0.95 / Math.max((b[1][0] - b[0][0]) / width,
+	    (b[1][1] - b[0][1]) / height),
+	t = [(width - s * (b[1][0] + b[0][0])) / 2,
+	    (height - s * (b[1][1] + b[0][1])) / 2];
+    projection.scale(s).translate(t);
+    // population map zoom listener
+    var pmzl = d3.behavior.zoom()
+	.scaleExtent([0.2, 8])
+	.on('zoom', pmzh);
+    canvas = d3.select('#pop_map_panel').
+	append('svg').
+	attr('width', width).
+	attr('height', height).
+	call(pmzl).
+	append('g').
+	attr('id', 'pm_canvas');
+    canvas.selectAll("path")
+        .data(town_boundary.features)
+    .enter()
+        .append("path")
+        .attr("d", path);
+
     d3.select(window).on('resize', refresh_all); 
     d3.selectAll('button.div-switch').on('click.refresh', refresh_all); 
     refresh_all();
+}
+
+function is_ready() {
+    return census_data!==null && town_boundary!==null;
+}
+    
+d3.json('census-taichung.json', function(error, data) {
+    if (error) return console.warn(error);
+    data.forEach(function (d) {
+	d['男'].unshift(0);
+	d['女'].unshift(0);
+    });
+    census_data = data;
+    if (is_ready()) init();
+});
+
+d3.json('town-boundary.json', function(error, data) {
+    if (error) return console.warn(error);
+    town_boundary = data;
+    if (is_ready()) init();
 });
 
     // https://stackoverflow.com/questions/13808741/bar-chart-with-d3-js-and-an-associative-array
